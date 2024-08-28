b9e97580ca046305c7df1caab67aca9f1fbaf109
Manaswini Kundeti <manaswini.kundeti@gmail.com>
2024-08-28 12:43:57 -0500

    Implemented splash screen in both light & dark mode

--- app/src/main/java/com/example/composenews/MainActivity.kt
+++ app/src/main/java/com/example/composenews/MainActivity.kt
@@ -8,12 +8,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.composenews.ui.theme.ComposeNewsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             ComposeNewsTheme {
 
